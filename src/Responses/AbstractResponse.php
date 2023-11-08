0b57d38a9f757e33d41fc8f16cfed741ed4231a6
cleeve <mike.cleeve@protected.net>
2023-11-08 09:06:36 +0000

    Add response hydrate method

--- src/Responses/AbstractResponse.php
+++ src/Responses/AbstractResponse.php
@@ -1,7 +1,19 @@
 <?php
 namespace Cubex\ApiTransport\Responses;
 
+use Packaged\Helpers\Objects;
+
 abstract class AbstractResponse implements ApiResponse
 {
+  /**
+   * @param object $response
+   *
+   * @return $this
+   */
+  public function hydrate(object $response): static
+  {
+    Objects::hydrate($this, $response);
 
+    return $this;
+  }
 }
--- src/Responses/ApiResponse.php
+++ src/Responses/ApiResponse.php
@@ -3,5 +3,10 @@ namespace Cubex\ApiTransport\Responses;
 
 interface ApiResponse
 {
-
+  /**
+   * @param object $response
+   *
+   * @return $this
+   */
+  public function hydrate(object $response): static;
 }
